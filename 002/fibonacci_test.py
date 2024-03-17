a101f4c01d3bb7e96684c9a210f97cb88d16b1ba
James Robinson <github@epii.co.uk>
2024-03-17 14:30:15 -0300

    Add a test using the implementation.

--- 002/fibonacci_test.py
+++ 002/fibonacci_test.py
@@ -36,3 +36,24 @@ def test_can_calculate_fib_negative_fib():
 
 def test_can_pass_test():
     f.check_fibonacci()
+
+def naive_implementation(n: int) -> int:
+    memo = {}
+
+    def calc(n: int) -> int:
+        if n < 0:
+            raise Exception("This Fibonacci sequence is only defined for the natural numbers.")
+        if n == 0:
+            return 0
+        if n == 1:
+            return 0
+        if n == 2:
+            return 1
+        if n not in memo:
+            memo[n] = calc(n - 1) + calc(n - 2)
+        return memo[n]
+    return calc(n)
+
+def test_can_evaluate_fib():
+    for i in range(1, 101):
+        assert f.fibonacci(i) == naive_implementation(i)
