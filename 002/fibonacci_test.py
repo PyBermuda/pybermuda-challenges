f408460678868f69ef59dbc5d1f4bc1975507eb5
James Robinson <github@epii.co.uk>
2024-03-17 14:19:43 -0300

    Update test for Gareth's non zero based indexing.

--- 002/fibonacci_test.py
+++ 002/fibonacci_test.py
@@ -2,28 +2,37 @@ import pytest
 import fibonacci as f
 
 def test_can_calculate_fib_0():
-    assert f.fibonacci(0) == 0  
+    assert f.fibonacci(1) == 0  
 
 def test_can_calculate_fib_1():
-    assert f.fibonacci(1) == 1  
-
-def test_can_calculate_fib_2():
     assert f.fibonacci(2) == 1  
 
+def test_can_calculate_fib_2():
+    assert f.fibonacci(3) == 1  
+
 def test_can_calculate_fib_3():
-    assert f.fibonacci(3) == 2  
+    assert f.fibonacci(4) == 2  
 
 def test_can_calculate_fib_4():
-    assert f.fibonacci(4) == 3  
+    assert f.fibonacci(5) == 3  
 
 def test_can_calculate_fib_5():
-    assert f.fibonacci(5) == 5  
+    assert f.fibonacci(6) == 5  
 
 def test_can_calculate_fib_6():
-    assert f.fibonacci(6) == 8  
+    assert f.fibonacci(7) == 8  
 
 def test_can_calculate_fib_negative_fib():
-    expected_message = "The Fibonacci sequence is only defined for whole numbers."
+    expected_message = "This Fibonacci sequence is only defined for the natural numbers."
+    with pytest.raises(Exception) as e:
+        f.fibonacci(0)
+    assert str(e.value) == expected_message
+
+def test_can_calculate_fib_negative_fib():
+    expected_message = "This Fibonacci sequence is only defined for the natural numbers."
     with pytest.raises(Exception) as e:
         f.fibonacci(-3)
     assert str(e.value) == expected_message
+
+def test_can_pass_test():
+    f.check_fibonacci()
