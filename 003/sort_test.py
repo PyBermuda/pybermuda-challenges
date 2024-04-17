ec0808c4f860f57b5cc5b757675f65da1c249bca
James Robinson <github@epii.co.uk>
2024-04-17 09:19:09 -0300

    Add alphabetical tests (should tehse be there).

--- 003/sort_test.py
+++ 003/sort_test.py
@@ -1,4 +1,6 @@
 from sort import sort
+import random
+
 
 def test_can_sort_list_in_ascending_order():
     list = [5, 3, 8, 2, 1, 4]
@@ -7,3 +9,8 @@ def test_can_sort_list_in_ascending_order():
 def test_can_sort_list_in_descending_order():
     list = [5, 3, 8, 2, 1, 4]
     assert sort(list, order='desc') == [8, 5, 4, 3, 2, 1]
+
+def test_sorting_alphabetical_order():
+    list = ['a', 'B', 'd', 'C']
+    assert sort(list, order='asc') == ['B', 'C', 'a', 'd']
+    assert sort(list, order='desc') == ['d', 'a', 'C', 'B']
