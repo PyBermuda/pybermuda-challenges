da9f5a1eac0359d37c00571e23cb99ef73687a69
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2024-04-18 12:24:53 -0300

    add test to prevent use of sort or sorted

--- 003/sort.py
+++ 003/sort.py
@@ -7,16 +7,25 @@ This excercise will introduce the concept of unit tests.
 
 The unit tests can be found in the file 003/test_sort.py
 
-To run the tests simply run the following command from inside the 003 directory:
-
-pytest .
+To run the tests simply run `pytest` in the terminal (after having done `pip install -r requirements.txt`)
 """
 
 from time import perf_counter
 
 
-def sort(list, *, order: str) -> int:
-    # write your code here 👇👇
-    pass
-    # write your code here 👆👆
+def manual_sort(list: list, *, order: str) -> int:
+    """
+    Sorts a list in ascending or descending order based on the given order parameter.
 
+    Args:
+        list: The list to be sorted.
+        order: The order in which to sort the list. Can be either "asc" for ascending order or "desc" for descending order.
+
+    Returns:
+        The sorted list.
+
+    """
+    # WRITE YOUR CODE HERE 👇👇
+    list.sort(reverse=order == "desc")
+    return list
+    # WRITE YOUR CODE HERE 👆👆
--- 003/test_sort.py
+++ 003/test_sort.py
@@ -1,19 +1,22 @@
-from sort import sort
+from sort import manual_sort
 import random
 
 
 def test_can_sort_list_in_ascending_order():
     list = [5, 3, 8, 2, 1, 4]
-    assert sort(list, order='asc') == [1, 2, 3, 4, 5, 8]
+    assert manual_sort(list, order="asc") == [1, 2, 3, 4, 5, 8]
+
 
 def test_can_sort_list_in_descending_order():
     list = [5, 3, 8, 2, 1, 4]
-    assert sort(list, order='desc') == [8, 5, 4, 3, 2, 1]
+    assert manual_sort(list, order="desc") == [8, 5, 4, 3, 2, 1]
+
 
 def test_sorting_alphabetical_order():
-    list = ['a', 'B', 'd', 'C']
-    assert sort(list, order='asc') == ['B', 'C', 'a', 'd']
-    assert sort(list, order='desc') == ['d', 'a', 'C', 'B']
+    list = ["a", "B", "d", "C"]
+    assert manual_sort(list, order="asc") == ["B", "C", "a", "d"]
+    assert manual_sort(list, order="desc") == ["d", "a", "C", "B"]
+
 
 def test_sort_really_long_list():
     size = 1_000_000
@@ -21,9 +24,10 @@ def test_sort_really_long_list():
     for i in range(size):
         list.append(random.randint(0, 1_000_000_000))
 
-    sorted = sort(list, order='asc')
+    sorted = manual_sort(list, order="asc")
     for i in range(1, size):
         assert sorted[i - 1] <= sorted[i]
+
 
 def test_sort_really_long_list_including_negative_desc():
     size = 1_000_000
@@ -31,6 +35,17 @@ def test_sort_really_long_list_including_negative_desc():
     for i in range(size):
         list.append(random.randint(-1_000_000_000, 1_000_000_000))
 
-    sorted = sort(list, order='desc')
+    sorted = manual_sort(list, order="desc")
     for i in range(1, size):
         assert sorted[i - 1] >= sorted[i]
+
+
+def test_sort_does_not_use_sort_or_sorted():
+    with open("003/sort.py") as file:
+        content = file.read()
+        assert (
+            "sorted(" not in content
+        ), "You are not allowed to use the sorted function. Write your own sorting algorithm."
+        assert (
+            ".sort(" not in content
+        ), "You are not allowed to use the .sort method. Write your own sorting algorithm."
